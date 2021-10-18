f0e7691e19e49c04ca6eb31ed1d5240f1abe0479
YiHsuan Lin <yihsuanlin.dolores@gmail.com>
2021-10-18 20:58:19 +0800

    Establish tabBarItem and navigationItem of HomeViewController

---
+++ toPether/Controller/HomeViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  toPether
+//
+//  Created by 林宜萱 on 2021/10/18.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // MARK: Navigation controller
+        self.navigationItem.title = "toPether"
+//        self.navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.font: UIFont(name: "PingFangTC-Medium", size: 24)]
+        
+        
+        
+    }
+
+
+}
+
