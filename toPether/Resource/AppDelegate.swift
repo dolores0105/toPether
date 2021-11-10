9aff1359984eb5089317e67aac77ded6a1ec9247
YiHsuan Lin <yihsuanlin.dolores@gmail.com>
2021-11-10 11:46:46 +0800

    View notification in the foreground

--- toPether/Resource/AppDelegate.swift
+++ toPether/Resource/AppDelegate.swift
@@ -46,6 +46,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             }
         }
         
+        UNUserNotificationCenter.current().delegate = self
+        
         return true
     }
 
@@ -61,5 +63,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+}
+
+extension AppDelegate: UNUserNotificationCenterDelegate {
+    func userNotificationCenter(_ center: UNUserNotificationCenter,
+                                willPresent notification: UNNotification,
+                                withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.badge, .sound, .banner])
     }
 }
