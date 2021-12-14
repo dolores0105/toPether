766b6d028fc75f1304c3f64fcc2d44d2d3e5ecc9
YiHsuan Lin <yihsuanlin.dolores@gmail.com>
2021-12-14 10:06:19 +0800

    Refactor whitespace

--- toPether/Extension/UITextFieldExtension.swift
+++ toPether/Extension/UITextFieldExtension.swift
@@ -8,12 +8,12 @@
 import UIKit
 
 extension UITextField {
-    func setLeftPaddingPoints(amount:CGFloat){
+    func setLeftPaddingPoints(amount: CGFloat) {
         let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: amount, height: self.frame.size.height))
         self.leftView = paddingView
         self.leftViewMode = .always
     }
-    func setRightPaddingPoints(amount:CGFloat) {
+    func setRightPaddingPoints(amount: CGFloat) {
         let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: amount, height: self.frame.size.height))
         self.rightView = paddingView
         self.rightViewMode = .always
